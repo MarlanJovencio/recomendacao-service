2db9e6b5e333cc505026bb3fa84faa4faae7d720
Marlan Jovencio <marlan.jovencio@itixti.com.br>
2024-09-19 20:45:21 -0300

    feat: versão recebendo a IReproducaoService

---
+++ src/main/java/org/ufes/gqs/recomendacaoservice/services/RecomendacaoService.java
@@ -0,0 +1,96 @@
+package org.ufes.gqs.recomendacaoservice.services;
+
+import br.com.interfaces.model.IHistoricoReproducao;
+import br.com.interfaces.model.IMusica;
+import br.com.interfaces.model.IPlaylist;
+import br.com.interfaces.model.IUsuario;
+import br.com.interfaces.repository.IHistoricoReproducaoRepository;
+import br.com.interfaces.repository.IMusicaRepository;
+import br.com.interfaces.repository.IUsuarioRepository;
+import br.com.interfaces.services.IRecomendacaoService;
+import br.com.interfaces.services.IReproducaoService;
+import br.com.repositories.HistoricoReproducaoRepository;
+import br.com.repositories.MusicaRepository;
+import br.com.repositories.UsuarioRepository;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class RecomendacaoService implements IRecomendacaoService {
+
+	private final IMusicaRepository musicaRepository;
+	private final IHistoricoReproducaoRepository historicoReproducaoRepository;
+	private final IUsuarioRepository usuarioRepository;
+
+	public RecomendacaoService() {
+		this.musicaRepository = MusicaRepository.getMusicaRepository();
+		historicoReproducaoRepository = HistoricoReproducaoRepository.getHistoricoReproducaoRepository();
+		usuarioRepository = UsuarioRepository.getUsuarioRepository();
+	}
+
+	@Override
+	public List<IMusica> recomendarMusicas( IUsuario usuario ) {
+		var oListHistoricoReproducao = historicoReproducaoRepository.findAllByUsuario( usuario );
+		if( oListHistoricoReproducao.isEmpty() ) {
+			return Collections.emptyList();
+		}
+		var listHistoricoReproducao = oListHistoricoReproducao.get();
+		var oMaisTocada = listHistoricoReproducao.stream().max( Comparator.comparingInt( IHistoricoReproducao::getQuantidadeVezesTocadas ) );
+		if( oMaisTocada.isEmpty() ) {
+			return Collections.emptyList();
+		}
+		var maisTocada = oMaisTocada.get();
+		return musicaRepository.findMaisTocadasByGenero( maisTocada.getMusica().getGenero(), 11 ).orElse( Collections.emptyList() ).stream().filter( m -> !maisTocada.getMusica().getTitulo().equals( m.getTitulo() ) ).toList();
+	}
+
+	@Override
+	public void registrarReproducao( IMusica musica, IUsuario usuario ) {
+		musicaRepository.findByTitulo( musica.getTitulo() ).orElseThrow( () -> new NoSuchElementException( "Música não encontrada" ) );
+		usuarioRepository.findByNome( usuario.getNome() ).orElseThrow( () -> new NoSuchElementException( "Usuario não encontrado" ) );
+		historicoReproducaoRepository.registrarReproducao( musica, usuario );
+	}
+
+	@Override
+	public List<IMusica> recomendarMusicasBaseadoNoHistorico( IReproducaoService reproducaoService, IUsuario usuario ) {
+		var oListHistoricoReproducao = historicoReproducaoRepository.findAllByUsuario( usuario );
+		if( oListHistoricoReproducao.isEmpty() ) {
+			return Collections.emptyList();
+		}
+		var listHistoricoReproducao = oListHistoricoReproducao.get();
+		var musicasMaisTocadas = listHistoricoReproducao.stream().sorted( Comparator.comparingInt( IHistoricoReproducao::getQuantidadeVezesTocadas ).reversed() ).map( IHistoricoReproducao::getMusica ).toList();
+		var generosMaisTocados = generosMaisTocados( musicasMaisTocadas );
+		var artistasMaisTocados = artistasMaisTocados( musicasMaisTocadas );
+		List<IMusica> list = new ArrayList<>();
+		generosMaisTocados.forEach( genero -> list.addAll( musicaRepository.findMaisTocadasByGenero( genero, 5 ).orElse( Collections.emptyList() ) ) );
+		artistasMaisTocados.forEach( artista -> list.addAll( musicaRepository.getMusicasMaisTocadas( artista, 5 ).orElse( Collections.emptyList() ) ) );
+		return list.stream().distinct().limit( 10 ).toList();
+	}
+
+	@Override
+	public List<IMusica> recomendarMusicasParaPlayList( IPlaylist playlist, IReproducaoService reproducaoService ) {
+		var generosMaisTocados = generosMaisTocados( playlist.getMusicas() );
+		var artistasMaisTocados = artistasMaisTocados( playlist.getMusicas() );
+		List<IMusica> list = new ArrayList<>();
+		generosMaisTocados.forEach( genero -> list.addAll( musicaRepository.findMaisTocadasByGenero( genero, 5 ).orElse( Collections.emptyList() ) ) );
+		artistasMaisTocados.forEach( artista -> list.addAll( musicaRepository.getMusicasMaisTocadas( artista, 5 ).orElse( Collections.emptyList() ) ) );
+
+		for( IUsuario colaborador : playlist.getColaboradores() ) {
+			list.addAll( recomendarMusicas( colaborador ) );
+		}
+
+		var musciasDistintas = list.stream().filter( iMusica -> playlist.getMusicas().stream().noneMatch( m -> m.getTitulo().equalsIgnoreCase( iMusica.getTitulo() ) ) ).distinct().limit( 10 ).toList();
+		if( musciasDistintas.isEmpty() ) {
+			return musicaRepository.getAll().stream().filter( iMusica -> playlist.getMusicas().stream().noneMatch( m -> m.getTitulo().equalsIgnoreCase( iMusica.getTitulo() ) ) ).sorted( Comparator.comparingInt( IMusica::getQtdVezesReproduzidas ).reversed() ).limit( 10 ).toList();
+		}
+		return musciasDistintas;
+	}
+
+	private List<String> generosMaisTocados( List<IMusica> musicas ) {
+		return musicas.stream().map( IMusica::getGenero ).collect( Collectors.groupingBy( g -> g, Collectors.counting() ) ).entrySet().stream().sorted( ( es1, es2 ) -> Long.compare( es2.getValue(), es1.getValue() ) ).map( Map.Entry::getKey ).toList();
+	}
+
+	private List<String> artistasMaisTocados( List<IMusica> musicas ) {
+		return musicas.stream().map( IMusica::getArtista ).collect( Collectors.groupingBy( g -> g, Collectors.counting() ) ).entrySet().stream().sorted( ( es1, es2 ) -> Long.compare( es2.getValue(), es1.getValue() ) ).map( Map.Entry::getKey ).toList();
+	}
+
+}
